983f21034578ae837aff2c281761b42bc24275a6
mvrublebsk <mvrublevsk@student.ucode.world>
2022-02-23 15:53:23 +0200

    first vers

--- inc/main.h
+++ inc/main.h
@@ -5,13 +5,6 @@
 #ifndef PROCESS_LAUNCHER_MAIN_H
 #define PROCESS_LAUNCHER_MAIN_H
 
-//
-// Created by Mark on 17.02.2022.
-//
-
-#ifndef PROCESS_LAUNCHER_MAIN_H
-#define PROCESS_LAUNCHER_MAIN_H
-
 #include <unistd.h>
 #include <iostream>
 #include <string>
@@ -23,39 +16,21 @@ enum STATUS {
     FAIL
 };
 
-class Process {
-private:
-    std::string executable_path;
-    std::vector<std::string> cmd_args {"privet"};
-public:
-    Process() : executable_path("../apps/a.out") {
-
-    }
-
-    std::string &get_path(void) {
-        return executable_path;
-    }
-
-    std::string &get_args(void) {
-        return cmd_args[0];
-    }
-};
-
-
 class LauncherViewer {
 private:
     std::string log_file_name;
 
 public:
-    void startProcess(Process process) {
+    void startProcess(ConfigFile cfg) {
         STATUS stat = RUNNING;
         int pid = fork();
 
         if(pid) {
-            const char *path = process.get_path().c_str();
-            const char *arg = process.get_args().c_str();
+            std::string path = cfg.get_processes()[0].get_executable_path();
+            std::string arg = cfg.get_processes()[0].get_cmd_arguments()[0].get_option_name();
 
-            execl(path, arg, (char *)NULL);
+
+            execl(path.c_str(), arg.c_str(), (char *)NULL);
 
             stat = FAIL;
             std::cout << stat << std::endl;
--- src/main.cpp
+++ src/main.cpp
@@ -2,8 +2,16 @@
 // Created by Mark on 17.02.2022.
 //
 
+
+#include "config.cpp"
 #include "main.h"
 
 int main(void) {
-
+    ConfigFile config_file;
+    LauncherViewer viewer;
+    config_file = parseYAMLConfig(loadYaml());
+    viewer.startProcess(config_file);
+//    std::cout << config_file.get_processes()[0].get_cmd_arguments()[0] << std::endl;
+//    std::cout << config_file.get_processes()[1].get_stdout_config() << std::endl;
+//    std::cout<<config_file<<std::endl;
 }
